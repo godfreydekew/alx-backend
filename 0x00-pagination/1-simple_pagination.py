e799f5d28efbcc27ca5730e40800e007d7ec10ec
godfreydekew <dekewgodfrey@gmail.com>
2024-07-21 15:01:34 +0000

    Add imports

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,18 +1,12 @@
-from typing import Tuple
+#!/usr/bin/env python3
+'''
+1. Simple pagination
+'''
 import csv
 import math
-from typing import List
+from typing import List, Tuple
 
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    '''
-    return a tuple of size two containing a start index and an end
-    index corresponding to the range of indexes to return in a list
-    for those particular pagination parameters.
-    '''
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:

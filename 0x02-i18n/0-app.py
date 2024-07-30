f2a825cd46b3dab01538b7756e2763589ee4f315
godfreydekew <dekewgodfrey@gmail.com>
2024-07-30 15:46:33 +0000

    Task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""BAsic Flask App"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Renders the main page"""
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    """Runs the program if its not imported"""
+    app.run(debug=True)

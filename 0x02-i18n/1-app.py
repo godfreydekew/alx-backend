33eb8b65d346980c55afb380d33723445ddf315c
godfreydekew <dekewgodfrey@gmail.com>
2024-07-30 16:44:49 +0000

    Task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Flask app with Babel for i18n support.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Configuration for Flask app.
+
+    Attributes:
+        LANGUAGES (list): List of available languages.
+        BABEL_DEFAULT_LOCALE (str): Default locale for translations.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone for the app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index() -> str:
+    """Render the index page with a welcome message."""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

897ccae03e9f69117ef2e73026fdedce33ec89bc
godfreydekew <dekewgodfrey@gmail.com>
2024-08-28 20:40:42 +0000

    Task 7

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,15 @@
+const kue = require('kue');
+
+// Create a queue
+const queue = kue.createQueue();
+
+
+function sendNotification(phoneNumber, message) {
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`)
+}
+
+queue.process('push_notification_code', function(job, done) {
+    const {phoneNumber, message} = job.data;
+    sendNotification(phoneNumber, message);
+    done();
+})
